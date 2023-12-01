c4f5b1077b62bf8dec0662cbc369ff87a401a398
Michal Goryszewski <goryszewski@gmail.com>
2023-12-01 20:07:00 +0100

    fix

--- server.py
+++ server.py
@@ -1,23 +1,23 @@
-    from flask import Flask
-    import redis
+from flask import Flask
+import redis
 
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    r = redis.Redis(host='redis-svc', port=6379, decode_responses=True)
+r = redis.Redis(host='redis-svc', port=6379, decode_responses=True)
 
-    @app.route('/')
-    def hello_world():
-        return 'Hello '
+@app.route('/')
+def hello_world():
+    return 'Hello '
 
-    @app.route('/set/<key>/<data>')
-    def set(key,data):
-        r.set(key,data )
-        return 'Set Redis data {}:{}'.format(key,data)
+@app.route('/set/<key>/<data>')
+def set(key,data):
+    r.set(key,data )
+    return 'Set Redis data {}:{}'.format(key,data)
 
-    @app.route('/get/<key>')
-    def get(key):
-        data = r.get(key)
-        return 'Get Redis data {}:{}'.format(key,data)
-    
-    if __name__ == '__main__':
-        app.run()
+@app.route('/get/<key>')
+def get(key):
+    data = r.get(key)
+    return 'Get Redis data {}:{}'.format(key,data)
+
+if __name__ == '__main__':
+    app.run()

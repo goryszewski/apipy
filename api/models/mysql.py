f485a3116489496ccf571c11fb1d97738cbf1aa8
Michal Goryszewski <goryszewski@gmail.com>
2023-12-29 23:27:16 +0100

    params get and add

--- api/models/mysql.py
+++ api/models/mysql.py
@@ -14,8 +14,22 @@ class DB:
         sql="show databases;"
         return self._exec_sql(sql)
 
+    def get_params(self,paramType):
+        sql="select * from params where paramType =?"
+        return self._exec_sql_tuple(self,sql,paramType)
+
+    def add_params(self,paramType,paramValue,modifyDate):
+        sql = """
+                insert into `params` (paramType,paramValue,modifyDate)
+                values (?, ?, ?)
+              """
+        dataTuple=(paramType,paramValue,modifyDate)
+        return self._exec_sql_tuple(sql,dataTuple)
+
+    def _exec_sql_tuple(self,sql,dataTuple):
+        return  self.cur.execute(sql,dataTuple)
+
     def _exec_sql(self,sql):
-        self.cur.execute(sql)
         return  self.cur.execute(sql)
 
     def __del__(self):

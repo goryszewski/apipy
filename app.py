e982d43ea233cc65bc622c77aecd28ca8ab62964
Michal Goryszewski <goryszewski@gmail.com>
2024-01-11 22:18:44 +0100

    cors fix

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ from flask import Flask
 from databases.db import initialize_db
 from flask_restful import Api
 from resources.routes import initialize_routes
+from flask_cors import CORS, cross_origin
+
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
-app.config['MONGODB_SETTINGS'] = {'host': 'mongodb://mongo/tasks'}
+app.config["MONGODB_SETTINGS"] = {"host": "mongodb://mongo/tasks"}
 initialize_db(app)
 initialize_routes(api)
 
--- databases/models.py
+++ databases/models.py
@@ -1,6 +1,17 @@
 from .db import db
 
+
 class Task(db.Document):
     name = db.StringField(required=True, unique=True)
     describe = db.StringField(required=True)
+    autor = db.StringField(required=True)
+
+
+class Project(db.Document):
+    name = db.StringField(required=True, unique=True)
+
+
+class Images(db.Document):
+    name = db.StringField(required=True, unique=True)
+    version = db.StringField(required=True)
     autor = db.StringField(required=True)

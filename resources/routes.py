ff5fb5452c844e29212dc823346aee40356cac8f
Michal Goryszewski <goryszewski@gmail.com>
2024-02-24 20:40:14 +0100

    Add Health Check

--- resources/routes.py
+++ resources/routes.py
@@ -2,8 +2,11 @@ from .task import TasksApi, TaskAPI
 
 from .project import ProjectsApi, ProjectAPI
 
+from .default import Default
+
 
 def initialize_routes(api):
+    api.add_resource(Default, "/readyz", "/livez")
     api.add_resource(TasksApi, "/api/tasks")
     api.add_resource(TaskAPI, "/api/tasks/<index>")
 
